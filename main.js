b3cdbc1d5682650c03699481dd7e52a916963f3e
zena <e.nosazena@gmail.com>
2020-05-13 13:54:46 -0400

    my own ui changes plus footer

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-var mymap = L.map('mapid').setView([55.70926, -3.87980], 10);
+var mymap = L.map('mapid').setView([37.7749, -122.4194], 10);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
-            L.marker([55.70926, -3.87980]).addTo(mymap)
+            L.marker([37.7749, -122.4194]).addTo(mymap)
 		    .bindPopup('our farm')
 		    .openPopup();
-$(window).trigger('resize');    
+$(window).trigger('resize');   
+
+
+  function topFunction() 
+  {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  } 
